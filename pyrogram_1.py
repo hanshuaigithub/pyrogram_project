316ec765f6e23af37a8e06d6ceffa3a85556cdc7
Hanshuai <792618173@qq.com>
2021-01-17 23:29:08 +0800

    Create pyrogram_1.py

---
+++ pyrogram_1.py
@@ -0,0 +1,10 @@
+from pyrogram import Client
+
+api_id = 2675972
+api_hash = "d4c1cfcea1e35a793443560545ed3318"
+
+# with Client("+639272572955", api_id, api_hash) as app:
+#     app.send_message("me", "Greetings from **Pyrogram**!")
+
+app = Client("+639272572955", api_id, api_hash)
+app.run()
