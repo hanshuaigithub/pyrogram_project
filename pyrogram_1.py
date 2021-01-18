abe95a1df3e636b9dfe8352bae1ca60a2e3eefc0
Hanshuai <792618173@qq.com>
2021-01-18 23:35:31 +0800

    Update pyrogram_1.py

--- pyrogram_1.py
+++ pyrogram_1.py
@@ -10,9 +10,8 @@ api_hash = "d4c1cfcea1e35a793443560545ed3318"
 # app.run()
 
 app = Client("+639272572955", api_id, api_hash)
-target = "pyrogramdemos"  # Target channel/supergroup
+target = "pyrogramlounge"  # Target channel/supergroup
 
 with app:
-    # for member in app.iter_chat_members(target):
-    #     print(member.user)
-    app.add_chat_members(target, "@snake9936")
+    for member in app.iter_chat_members(target):
+        print(member.user)
