5f4cf08f43793944daa60e5f689f08bee224335c
Hanshuai <792618173@qq.com>
2021-01-18 22:48:03 +0800

    xx

--- pyrogram_1.py
+++ pyrogram_1.py
@@ -3,8 +3,15 @@ from pyrogram import Client
 api_id = 2675972
 api_hash = "d4c1cfcea1e35a793443560545ed3318"
 
-with Client("+639272572955", api_id, api_hash) as app:
-    app.send_message("me", "Greetings from **Pyrogram**!")
+# with Client("+639272572955", api_id, api_hash) as app:
+#     app.send_message("me", "Greetings from **Pyrogram**!")
 
 # app = Client("+639272572955", api_id, api_hash)
 # app.run()
+
+app = Client("+639272572955", api_id, api_hash)
+target = "https://t.me/pyrogramlounge"  # Target channel/supergroup
+
+with app:
+    for member in app.iter_chat_members(target):
+        print(member.user.first_name)
