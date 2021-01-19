5949bb7bce5a4065c496fa163c0e45ac8b219679
Hanshuai <792618173@qq.com>
2021-01-19 21:09:52 +0800

    Update pyrogram_1.py

--- pyrogram_1.py
+++ pyrogram_1.py
@@ -10,13 +10,15 @@ api_hash = "d4c1cfcea1e35a793443560545ed3318"
 # app.run()
 
 app = Client("+639272572955", api_id, api_hash)
-target = "pyrogramlounge"  # Target channel/supergroup
+target = "sigui588"  # Target channel/supergroup
 
 with app:
     members = app.iter_chat_members(target)
     for i in range(0,20):
         member = members[i]
-        print(member.user)
+        print(member.user.first_name)
+        if member.user.username != "TylerMatthew":
+            app.add_chat_members("pyrogramdemos", member.user.id)
     
     # app.add_chat_members("pyrogramdemos", "@jk123jk456")
     # app.add_chat_members("pyrogramdemos", "@snake9936")
