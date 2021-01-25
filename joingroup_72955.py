6aa9801e2a5814fd74edd48565c6028ef5c57bed
Hanshuai <792618173@qq.com>
2021-01-25 21:50:39 +0800

    xx

--- joingroup_72955.py
+++ joingroup_72955.py
@@ -1,11 +1,15 @@
 from pyrogram import Client
 from pyrogram.errors import RPCError
 import json
+import sys
+import time
 
 api_id = 2675972
 api_hash = "d4c1cfcea1e35a793443560545ed3318"
-
 app = Client("+639272572955", api_id, api_hash)
+
+fromIndex = sys.argv[1]
+toIndex = sys.argv[2]
 
 with app:
     f = open("members.json")
@@ -13,13 +17,19 @@ with app:
     f.close()
     members = json.loads(members_json_str)
 
-    for i in range(80,100):
+    for i in range(fromIndex,toIndex):
         member = members[i]
         print(f"adding :{member['first_name']}")
         try:
             if app.add_chat_members("sexchatcn", member['id']):
                 print(f"success...[{i}]")
+                time.sleep(10)
         except RPCError as e:
             print(f"err :{e.MESSAGE}")
+            if e.MESSAGE == 'The method can\'t be used because your account is currently limited':
+                print("[sleep 30 mins...]")
+                time.sleep(1*60*30)
+            else:
+                time.sleep(10)
 
 
---
+++ test.py
@@ -0,0 +1,5 @@
+import time
+
+for i in range(0,10):
+    print(f">>>>>{i}")
+    time.sleep(2)
