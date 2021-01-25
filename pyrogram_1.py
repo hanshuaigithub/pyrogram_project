5f13b3efd4dfecc75dc2c2793ea48f022086bfa5
Hanshuai <792618173@qq.com>
2021-01-25 15:05:48 +0800

    Update pyrogram_1.py

--- pyrogram_1.py
+++ pyrogram_1.py
@@ -5,17 +5,18 @@ api_id = 2675972
 api_hash = "d4c1cfcea1e35a793443560545ed3318"
 
 app = Client("+639272572955", api_id, api_hash)
-target = "sigui588"  # Target channel/supergroup
+target = "cnsex8"  # Target channel/supergroup sigui588
 
 with app:
     members = app.iter_chat_members(target)
     print(f"Chanel members counts : {len(members)}")
-    for i in range(100,200):
-        member = members[i]
-        print(f"adding :{member.user.first_name}")
-        try:
-            if app.add_chat_members("pyrogramdemos", member.user.id):
-                print("success...")
-        except RPCError as e:
-            print(f"err :{e.MESSAGE}")
+    # for i in range(100,200):
+    #     member = members[i]
+    #     print(f"adding :{member.user.first_name}")
+    #     try:
+    #         if app.add_chat_members("pyrogramdemos", member.user.id):
+    #             print("success...")
+    #     except RPCError as e:
+    #         print(f"err :{e.MESSAGE}")
+
 
