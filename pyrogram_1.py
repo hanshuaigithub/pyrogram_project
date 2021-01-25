2050806deab27fbbcf71afa0e1f47e9e43e0a6b7
Hanshuai <792618173@qq.com>
2021-01-25 16:30:19 +0800

    Update pyrogram_1.py

--- pyrogram_1.py
+++ pyrogram_1.py
@@ -1,5 +1,6 @@
 from pyrogram import Client
 from pyrogram.errors import RPCError
+import json
 
 api_id = 2675972
 api_hash = "d4c1cfcea1e35a793443560545ed3318"
@@ -10,13 +11,18 @@ target = "cnsex8"  # Target channel/supergroup sigui588
 with app:
     members = app.iter_chat_members(target)
     print(f"Chanel members counts : {len(members)}")
-    for i in range(0,100):
-        member = members[i]
-        print(f"adding :{member.user.first_name}")
-        try:
-            if app.add_chat_members("pyrogramdemos", member.user.id):
-                print(f"success...{i}")
-        except RPCError as e:
-            print(f"err :{e.MESSAGE}")
+    members_json_str = json.dumps(members)
+    members_open = open('members.json', 'w')
+    members_open.write(members_json_str)
+    members_open.close()
+
+    # for i in range(0,100):
+    #     member = members[i]
+    #     print(f"adding :{member.user.first_name}")
+    #     try:
+    #         if app.add_chat_members("pyrogramdemos", member.user.id):
+    #             print(f"success...{i}")
+    #     except RPCError as e:
+    #         print(f"err :{e.MESSAGE}")
 
 
