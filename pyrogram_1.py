70d02010584ff4bff447bc1fac0e51decc8aef7e
Hanshuai <792618173@qq.com>
2021-01-25 16:33:09 +0800

    Update pyrogram_1.py

--- pyrogram_1.py
+++ pyrogram_1.py
@@ -6,15 +6,16 @@ api_id = 2675972
 api_hash = "d4c1cfcea1e35a793443560545ed3318"
 
 app = Client("+639272572955", api_id, api_hash)
-target = "cnsex8"  # Target channel/supergroup sigui588
+target = "sigui588"  # Target channel/supergroup sigui588 cnsex8
 
 with app:
     members = app.iter_chat_members(target)
     print(f"Chanel members counts : {len(members)}")
-    members_json_str = json.dumps(members)
-    members_open = open('members.json', 'w')
-    members_open.write(members_json_str)
-    members_open.close()
+    print(members)
+    # members_json_str = json.dumps(members)
+    # members_open = open('members.json', 'w')
+    # members_open.write(members_json_str)
+    # members_open.close()
 
     # for i in range(0,100):
     #     member = members[i]
