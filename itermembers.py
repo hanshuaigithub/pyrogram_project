2a1f42a6475c7dd311ca87b9f74e63cfd2da4409
Hanshuai <792618173@qq.com>
2021-01-25 17:12:19 +0800

    xx

--- itermembers.py
+++ itermembers.py
@@ -1,5 +1,4 @@
 from pyrogram import Client
-from pyrogram.errors import RPCError
 import json
 
 api_id = 2675972
@@ -21,14 +20,5 @@ with app:
     members_open = open('members.json', 'w')
     members_open.write(members_json_str)
     members_open.close()
-
-    # for i in range(0,100):
-    #     member = members[i]
-    #     print(f"adding :{member.user.first_name}")
-    #     try:
-    #         if app.add_chat_members("pyrogramdemos", member.user.id):
-    #             print(f"success...{i}")
-    #     except RPCError as e:
-    #         print(f"err :{e.MESSAGE}")
 
 
---
+++ joingroup.py
@@ -0,0 +1,25 @@
+from pyrogram import Client
+from pyrogram.errors import RPCError
+import json
+
+api_id = 2675972
+api_hash = "d4c1cfcea1e35a793443560545ed3318"
+
+app = Client("+639272572955", api_id, api_hash)
+
+with app:
+    f = open("members.json")
+    members_json_str = f.read()
+    f.close()
+    members = json.loads(members_json_str)
+
+    for i in range(0,20):
+        member = members[i]
+        print(f"adding :{member.first_name}")
+        try:
+            if app.add_chat_members("sexchatcn", member.id):
+                print(f"success...{i}")
+        except RPCError as e:
+            print(f"err :{e.MESSAGE}")
+
+
