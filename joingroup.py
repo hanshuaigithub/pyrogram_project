5894a84f6f8f222ec18875738b6aaf9a7ad50f16
Hanshuai <792618173@qq.com>
2021-01-25 17:22:28 +0800

    Update joingroup.py

--- joingroup.py
+++ joingroup.py
@@ -13,7 +13,7 @@ with app:
     f.close()
     members = json.loads(members_json_str)
 
-    for i in range(20,40):
+    for i in range(40,60):
         member = members[i]
         print(f"adding :{member['first_name']}")
         try:
